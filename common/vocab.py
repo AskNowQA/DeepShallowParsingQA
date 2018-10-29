fade2d3b60bd711330c50c84e9f5808b0cebbad8
hamidzafar <hamid.zafar.tud@gmail.com>
2018-10-29 16:37:26 +0100

    use utf-8

--- common/vocab.py
+++ common/vocab.py
@@ -19,7 +19,7 @@ class Vocab(object):
     # Load entries from a file.
     def loadFile(self, filename):
         idx = 0
-        for line in open(filename):
+        for line in open(filename, encoding="utf-8"):
             token = line.rstrip('\n')
             self.add(token)
             idx += 1
