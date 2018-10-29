7292e266a8add2853ac7141c3961cff72433f53e
hamidzafar <hamid.zafar.tud@gmail.com>
2018-10-29 16:17:52 +0100

    build vocab for lc_quad

--- common/dataset/lc_quad.py
+++ common/dataset/lc_quad.py
@@ -1,16 +1,34 @@
+from common.vocab import Vocab
+
 import ujson as json
+import os
 
 
 class LC_QuAD:
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, vocab_path):
         with open(dataset_path, 'r') as file_hanlder:
             self.raw_dataset = json.load(file_hanlder)
 
-            self.corpus = [[self.__preprocess(item['corrected_question']),
-                            item['corrected_question'],
-                            item['annotation']] for item in
-                           self.raw_dataset]
-            self.validate = all([(len(item[0].split()) == len(item[1])) for item in self.corpus])
+            self.dataset = [[self.__preprocess(item['corrected_question']),
+                             item['corrected_question'],
+                             item['annotation']] for item in
+                            self.raw_dataset]
+            self.corpus = [item[0] for item in self.dataset]
+            self.validate = all([(len(item[0].split()) == len(item[2])) for item in self.dataset])
+            if self.validate:
+                if not os.path.isfile(vocab_path):
+                    self.__build_vocab(self.corpus, vocab_path)
+            self.vocab = Vocab(filename=vocab_path)
+            self.coded_corpus = [[self.vocab.getIndex(word) for word in item.split()] for item in self.corpus]
+
+    def __build_vocab(self, lines, vocab_path):
+        vocab = set()
+        for line in lines:
+            tokens = line.rstrip('\n').split(' ')
+            vocab |= set(tokens)
+        with open(vocab_path, 'w') as f:
+            for token in sorted(vocab):
+                f.write(token + '\n')
 
     def __preprocess(self, line):
-        return line.replace('?', ' ').replace('\'', ' ').replace('-', ' ')
+        return line.lower().replace('?', ' ').replace('\'', ' ').replace('-', ' ').replace(',', ' ')
