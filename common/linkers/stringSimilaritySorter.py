95ac79a543d5acbb82aaad5d12cab9f1d06fa610
hamidzafar <hamid.zafar.tud@gmail.com>
2018-11-05 13:23:53 +0100

    use jellyfish for speed-up

--- common/linkers/stringSimilaritySorter.py
+++ common/linkers/stringSimilaritySorter.py
@@ -1,10 +1,10 @@
-from difflib import SequenceMatcher
+import jellyfish
 
 
 class StringSimilaritySorter:
     def sort(self, surface, question, relations):
-        relations_similarity = [[relation, SequenceMatcher(None, surface, relation[1]).ratio()]
+        relations_similarity = [[relation, jellyfish.levenshtein_distance(surface.lower(), relation[1].lower())]
                                 for relation in relations]
-        relations_similarity.sort(key=lambda x: x[1], reverse=True)
+        relations_similarity.sort(key=lambda x: x[1])
 
         return [relation[0] for relation in relations_similarity]
