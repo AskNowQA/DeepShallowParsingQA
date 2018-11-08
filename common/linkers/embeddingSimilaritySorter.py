1e7438ae97ee0fe97abbf0adf67c0948a2f112b1
hamidzafar <hamid.zafar.tud@gmail.com>
2018-11-08 10:03:48 +0100

    refactor

--- common/linkers/embeddingSimilaritySorter.py
+++ common/linkers/embeddingSimilaritySorter.py
@@ -8,11 +8,11 @@ class EmbeddingSimilaritySorter:
 
     def sort(self, surface, question, candidates):
         surface_embeddings = self.word_vectorizer.decode(surface)
-        candidates_similarity = [[candidate,
-                                  np.mean(cosine_similarity(surface_embeddings,
-                                                            self.word_vectorizer.vectors[candidate[5]]))]
-                                 for candidate in candidates
+        candidates_embeddings = [[idx, surface_embeddings, self.word_vectorizer.vectors[candidate[5]]]
+                                 for idx, candidate in enumerate(candidates)
                                  if len(candidate[5]) > 0]
+        candidates_similarity = [[item[0], np.mean(cosine_similarity(item[1], item[2]))] for item in
+                                 candidates_embeddings]
         candidates_similarity.sort(key=lambda x: x[1], reverse=True)
 
-        return [candidate[0] for candidate in candidates_similarity]
+        return [candidates[item[0]] for item in candidates_similarity]
