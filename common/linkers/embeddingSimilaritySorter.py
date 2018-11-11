7c06191b0d162aedddcd390c9022157a1f512e1a
hamidzafar <hamid.zafar.tud@gmail.com>
2018-11-11 19:07:36 +0100

    fix cuda related bugs

--- common/linkers/embeddingSimilaritySorter.py
+++ common/linkers/embeddingSimilaritySorter.py
@@ -10,6 +10,8 @@ class EmbeddingSimilaritySorter:
         emb_shape = self.word_vectorizer.emb.shape
         self.emb = nn.Embedding(emb_shape[0], emb_shape[1], padding_idx=0, sparse=False)
         self.emb.weight.data.copy_(word_vectorizer.emb)
+        if torch.cuda.is_available():
+            self.emb.cuda()
 
     def sort(self, surface, question, candidates):
         surface_embeddings = self.word_vectorizer.decode(surface)
@@ -21,11 +23,15 @@ class EmbeddingSimilaritySorter:
         for idx, item in enumerate(tmp):
             candidates_coded[idx][:len(item)] = item
         if torch.cuda.is_available():
+            surface_embeddings = surface_embeddings.cuda()
             candidates_coded = candidates_coded.cuda()
             lens = lens.cuda()
         candidates_embeddings = self.emb(candidates_coded)
         candidates_embeddings_mean = torch.sum(candidates_embeddings, dim=1) / lens
         candidates_similarity = torch.nn.functional.cosine_similarity(surface_embeddings, candidates_embeddings_mean)
-        sorted_idx = np.argsort(candidates_similarity.data.numpy())[::-1]
+        candidates_similarity = candidates_similarity.data
+        if torch.cuda.is_available():
+            candidates_similarity = candidates_similarity.cpu()
+        sorted_idx = np.argsort(candidates_similarity.numpy())[::-1]
         sorted = [candidates[idx] for idx in sorted_idx]
         return sorted
