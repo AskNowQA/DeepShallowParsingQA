120aaa287828e30de1c55de1bb9830f5a43be17e
hamidzafar <hamid.zafar.tud@gmail.com>
2018-11-12 13:02:42 +0100

    add logger

--- common/linkers/orderedLinker.py
+++ common/linkers/orderedLinker.py
@@ -1,3 +1,4 @@
+import logging
 from common.linkers.unorderedLinker import UnorderedLinker
 
 
@@ -5,6 +6,7 @@ class OrderedLinker(UnorderedLinker):
     def __init__(self, rel2id_path, core_chains_path, sorter, dataset):
         super(OrderedLinker, self).__init__(rel2id_path, core_chains_path, dataset)
         self.sorter = sorter
+        self.logger = logging.getLogger('main')
 
     def link(self, surface, question):
         unordered_results = super(OrderedLinker, self).link(surface, question)
@@ -38,8 +40,9 @@ class OrderedLinker(UnorderedLinker):
         if k > 0 and max_len > 0:
             mrr = sum(map(lambda x: 1.0 / (x + 1), rank)) / max_len
         # if len(qarow.normalized_question) > len(surfaces[0]):
-        #     print(qarow.question,
-        #           qarow.normalized_question,
-        #           [self.dataset.vocab.convertToLabels(item) for item in surfaces],
-        #           [rel.raw_uri for rel in qarow.sparql.relations])
+        self.logger.debug([qarow.question,
+                           qarow.normalized_question,
+                           [self.dataset.vocab.convertToLabels(item) for item in surfaces],
+                           [rel.raw_uri for rel in qarow.sparql.relations]])
+
         return sum(scores) / max_len, mrr
--- scripts/execute.py
+++ scripts/execute.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import logging
 
 from config import config
 from scripts.config_args import parse_args
@@ -11,15 +12,23 @@ torch.manual_seed(6)
 torch.backends.cudnn.deterministic = True
 
 if __name__ == '__main__':
-
     args = parse_args()
-    print (args)
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("[%(asctime)s] %(levelname)s:%(name)s:%(message)s")
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.info(args)
+
     lc_quad = LC_QuAD(config['lc_quad']['train'], config['lc_quad']['test'], config['lc_quad']['vocab'],
                       args.remove_entity, args.remove_stop_words)
     runner = Runner(lc_quad, args)
 
     if args.mode == 'test':
+        logger.setLevel(logging.DEBUG)
         runner.load_checkpoint()
     else:
+
         runner.train(lc_quad, args)
     runner.test(lc_quad, args)
