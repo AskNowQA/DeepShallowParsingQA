c285e84c9b28c57ae35c369551e8a6751cc26ec6
hamidzafar <hamid.zafar.tud@gmail.com>
2018-11-30 10:31:52 +0100

    add elastic configs

--- config.py
+++ config.py
@@ -2,14 +2,58 @@ import os
 
 config = {
     'base_path': '/Users/hamid/workspace/DeepShallowParsingQA',
+    'elastic': {
+        'server': '127.0.0.1:9200',
+        'entity_index_config': {
+            'settings': {
+                'number_of_shards': 1,
+                'number_of_replicas': 0,
+                'analysis': {
+                    'filter': {
+                        'trigrams_filter': {
+                            'type': 'ngram',
+                            'min_gram': 2,
+                            'max_gram': 3
+                        }
+                    },
+                    'analyzer': {
+                        'trigrams': {
+                            'type': 'custom',
+                            'tokenizer': 'standard',
+                            'filter': ['lowercase', 'trigrams_filter']
+                        }
+                    }
+                }
+            },
+            'mappings': {'resources': {
+                'properties': {
+                    'label': {
+                        'type': 'text',
+                        'analyzer': 'trigrams'
+                    },
+                    'wikidata_label': {
+                        'type': 'text',
+                        'analyzer': 'trigrams'
+                    },
+                    'edge_count': {
+                        'type': 'integer'
+                    }
+                }
+            }}
+
+        }
+    }
 }
 config['data_path'] = os.path.join(config['base_path'], 'data')
 config['glove_path'] = os.path.join(config['data_path'], 'glove/glove.840B.300d')
 config['checkpoint_path'] = os.path.join(config['base_path'], 'checkpoint.chpt')
 
-config['lc_quad'] = {
-    'base_path': os.path.join(config['data_path'], 'lcquad')
+config['dbpedia'] = {'base_path': os.path.join(config['data_path'], 'dbpedia')}
+config['dbpedia'] = {
+    'entities': os.path.join(config['dbpedia']['base_path'], 'entities.json')
 }
+
+config['lc_quad'] = {'base_path': os.path.join(config['data_path'], 'lcquad')}
 config['lc_quad'] = {
     'tiny': os.path.join(config['lc_quad']['base_path'], 'tiny.json'),
     'train': os.path.join(config['lc_quad']['base_path'], 'train-data.json'),
