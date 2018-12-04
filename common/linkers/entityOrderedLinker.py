8442da0bf78f15c5bbaecc1ae473825be6d3c6c9
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-04 14:21:50 +0100

    in train mode, if intersection of ngrams of surface with the one of the target uri is higher than a threshold, it will query the candidate generator

--- common/linkers/entityOrderedLinker.py
+++ common/linkers/entityOrderedLinker.py
@@ -1,16 +1,32 @@
 from common.linkers.orderedLinker import OrderedLinker
+from common.utils import *
 
 
 class EntityOrderedLinker(OrderedLinker):
     def __init__(self, candidate_generator, sorters, vocab):
         super(EntityOrderedLinker, self).__init__(candidate_generator, sorters, vocab)
 
+    @profile
     def best_ranks(self, surfaces, qa_row, k, train):
-        results = super(EntityOrderedLinker, self).best_ranks(surfaces, qa_row.sparql.entities, qa_row.question, k, train)
+        if train:
+            for entity in qa_row.sparql.entities:
+                for surface in list(surfaces):
+                    string_surface = ' '.join(self.vocab.convertToLabels(surface))
+                    surface_ngram = Utils.ngrams(string_surface)
+                    if len(surface_ngram) == 0 or len(entity.ngram.intersection(surface_ngram)) / len(
+                            surface_ngram) < 0.7:
+                        surfaces.remove(surface)
 
-        self.logger.debug(qa_row.question)
-        self.logger.debug(qa_row.normalized_question)
+        results = super(EntityOrderedLinker, self).best_ranks(surfaces,
+                                                              qa_row.sparql.entities,
+                                                              qa_row.question,
+                                                              k,
+                                                              train)
+
+        # self.logger.debug(qa_row.question)
+        # self.logger.debug(qa_row.normalized_question)
         self.logger.debug([self.vocab.convertToLabels(item) for item in surfaces])
-        self.logger.debug([rel.raw_uri for rel in qa_row.sparql.relations])
+        self.logger.debug([rel.raw_uri for rel in qa_row.sparql.entities])
+        self.logger.debug('')
 
         return results
