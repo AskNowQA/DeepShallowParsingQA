1635d9a2b0c8c3fdff072cd7edd32c1e84ef69d9
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-04 23:27:46 +0100

    add size params

--- scripts/create_elastic_index.py
+++ scripts/create_elastic_index.py
@@ -6,10 +6,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create ElasticSearch Index')
     parser.add_argument('--create_index', dest='create_index', action='store_true')
     parser.add_argument('--search', default='search')
+    parser.add_argument('--size', default=10)
     args = parser.parse_args()
 
     e = Elastic(config['elastic']['server'],
                 config['elastic']['entity_index_config'],
                 config['dbpedia']['entities'],
                 create_entity_index=args.create_index)
-    print(e.search_ngram(args.search, 'idx'))
+    print(e.search_ngram(args.search, 'idx', size=args.size))
