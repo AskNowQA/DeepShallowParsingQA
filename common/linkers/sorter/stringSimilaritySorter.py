1573165933bee435baadd3aba4c38cf8da0aec53
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-04 14:23:18 +0100

    fix minor bug

--- common/linkers/sorter/stringSimilaritySorter.py
+++ common/linkers/sorter/stringSimilaritySorter.py
@@ -6,9 +6,11 @@ from common.utils import *
 class StringSimilaritySorter:
     @profile
     def sort(self, surface, question, candidates):
+        if candidates is None or len(candidates) == 0:
+            return []
         candidates_similarity = np.array([jellyfish.levenshtein_distance(surface, candidate[1].lower())
                                           for candidate in candidates])
-        max_distance = len(surface)
+        max_distance = len(surface) * 2
         filter_threshold = candidates_similarity < max_distance
         candidates_similarity = candidates_similarity[filter_threshold]
         filtered_candidates = np.array(candidates, dtype=object)[filter_threshold]
