6dc0770b3d9894aba7b71d339cb0ca96c62b351a
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-06 11:47:54 +0100

    add index_name and size

--- common/linkers/candidate_generator/ngramCG.py
+++ common/linkers/candidate_generator/ngramCG.py
@@ -1,6 +1,7 @@
 class NGramLinker:
-    def __init__(self, elastic):
+    def __init__(self, elastic, index_name):
         self.elastic = elastic
+        self.index_name = index_name
 
-    def generate(self, surface, question):
-        return self.elastic.search_ngram(surface, 'idx')
+    def generate(self, surface, question, size=10):
+        return self.elastic.search_ngram(surface, self.index_name, size=size)
