932bce9202595744f81f58363cb33a949f3ff821
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-06 11:17:54 +0100

    take index_name and size from args

--- scripts/create_elastic_index.py
+++ scripts/create_elastic_index.py
@@ -5,12 +5,14 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create ElasticSearch Index')
     parser.add_argument('--create_index', dest='create_index', action='store_true')
+    parser.add_argument('--index_name', default='idx')
     parser.add_argument('--search', default='search')
     parser.add_argument('--size', default=10)
     args = parser.parse_args()
 
     e = Elastic(config['elastic']['server'],
-                config['elastic']['entity_index_config'],
+                config['elastic']['entity_ngram_index_config'],
                 config['dbpedia']['entities'],
+                index_name=args.index_name,
                 create_entity_index=args.create_index)
-    print(e.search_ngram(args.search, 'idx', size=args.size))
+    print(e.search_ngram(args.search, args.index_name, size=args.size))
