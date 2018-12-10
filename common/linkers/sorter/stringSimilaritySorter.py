d961497714aad96136985ffd36e6377c01bcb14a
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-10 21:06:25 +0100

    fix minor bug

--- common/linkers/sorter/stringSimilaritySorter.py
+++ common/linkers/sorter/stringSimilaritySorter.py
@@ -14,13 +14,15 @@ class StringSimilaritySorter:
         candidates_distance = np.array(
             [(self.metric(surface, candidate[1].lower()) if len(candidate[1]) > 2 else 1)
              for candidate in candidates], dtype=float)
+        if self.return_similarity:
+            max_distance = np.max(candidates_distance)
         exact_match_idx = [idx for idx, candidate in enumerate(candidates) if surface in candidate[1].lower()]
         candidates_distance[exact_match_idx] /= 2
         candidates_distance = candidates_distance
         filtered_candidates = np.array(candidates, dtype=object)
         idxs = np.argsort(candidates_distance)
         if self.return_similarity:
-            candidates_similarity = 1 - (candidates_distance) / max(np.max(candidates_distance), len(surface))
+            candidates_similarity = 1 - (candidates_distance) / max(max_distance, len(surface))
             output = np.hstack((filtered_candidates[idxs], candidates_similarity[idxs].reshape(-1, 1)))
         else:
             output = filtered_candidates[idxs]
