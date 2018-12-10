e85a050ffce4f79ba445d86d3ef51c5714e0dd7a
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-10 15:57:07 +0100

    fix bug: if a surface is removed, MRR should be reduced

--- common/linkers/entityOrderedLinker.py
+++ common/linkers/entityOrderedLinker.py
@@ -3,11 +3,12 @@ from common.utils import *
 
 
 class EntityOrderedLinker(OrderedLinker):
-    def __init__(self, candidate_generator, sorters, vocab):
-        super(EntityOrderedLinker, self).__init__(candidate_generator, sorters, vocab)
+    def __init__(self, candidate_generator, sorters, vocab, include_similarrity_score=True):
+        super(EntityOrderedLinker, self).__init__(candidate_generator, sorters, vocab, include_similarrity_score)
 
     @profile
     def best_ranks(self, surfaces, qa_row, k, train):
+        number_of_removed_items = 0
         if train:
             for entity in qa_row.sparql.entities:
                 for surface in list(surfaces):
@@ -16,6 +17,7 @@ class EntityOrderedLinker(OrderedLinker):
                     if len(surface_ngram) == 0 or len(entity.ngram.intersection(surface_ngram)) / len(
                             surface_ngram) < 0.7:
                         surfaces.remove(surface)
+                        number_of_removed_items += 1
 
         results = super(EntityOrderedLinker, self).best_ranks(surfaces,
                                                               qa_row.sparql.entities,
@@ -23,6 +25,7 @@ class EntityOrderedLinker(OrderedLinker):
                                                               k,
                                                               train)
 
+        results = (results[0] / (number_of_removed_items + 1), results[1] / (number_of_removed_items + 1))
         # self.logger.debug(qa_row.question)
         # self.logger.debug(qa_row.normalized_question)
         self.logger.debug([' '.join(self.vocab.convertToLabels(item)) for item in surfaces])
