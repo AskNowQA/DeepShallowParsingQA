2e596279c2a1e74d386bd6ad6f48bd3ba20798e3
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-13 19:24:12 +0100

    fix bug

--- common/linkers/sorter/stringSimilaritySorter.py
+++ common/linkers/sorter/stringSimilaritySorter.py
@@ -12,17 +12,21 @@ class StringSimilaritySorter:
         if candidates is None or len(candidates) == 0:
             return []
         candidates_distance = np.array(
-            [(self.metric(surface, candidate[1].lower()) if len(candidate[1]) > 2 else 1)
+            [(self.metric(surface, candidate[1].lower()))
              for candidate in candidates], dtype=float)
-        if self.return_similarity:
-            max_distance = np.max(candidates_distance)
         exact_match_idx = [idx for idx, candidate in enumerate(candidates) if surface in candidate[1].lower()]
         candidates_distance[exact_match_idx] /= 2
         candidates_distance = candidates_distance
         filtered_candidates = np.array(candidates, dtype=object)
         idxs = np.argsort(candidates_distance)
         if self.return_similarity:
-            candidates_similarity = 1 - (candidates_distance) / max(max_distance, len(surface))
+            max_distance = np.max(candidates_distance)
+            if max_distance <= 1:
+                candidates_similarity = 1 - candidates_distance
+            else:
+                surface_len = len(surface)
+                candidates_len = np.array([max(surface_len, len(candidate[1])) for candidate in candidates])
+                candidates_similarity = 1 - (candidates_distance / candidates_len)
             output = np.hstack((filtered_candidates[idxs], candidates_similarity[idxs].reshape(-1, 1)))
         else:
             output = filtered_candidates[idxs]
