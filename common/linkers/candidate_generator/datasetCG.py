7db15eef1dbc1dcdcb2eaec13a6ff742f4081cf7
hamidzafar <hamid.zafar.tud@gmail.com>
2018-12-20 11:46:47 +0100

    extract entities and relations

--- common/linkers/candidate_generator/datasetCG.py
+++ common/linkers/candidate_generator/datasetCG.py
@@ -2,12 +2,19 @@ from common.utils import *
 
 
 class DatasetCG:
-    def __init__(self, dataset):
-        self.dataset = {item.question: [[entity.raw_uri, entity.label] for entity in item.sparql.entities] for item in
-                        dataset.train_set + dataset.test_set}
+    def __init__(self, dataset, entity=False, relation=False):
+        self.idx = 0
+        if entity:
+            self.idx = 0
+        elif relation:
+            self.idx = 1
+        self.dataset = {item.question: [
+            [[entity.raw_uri, entity.label] for entity in item.sparql.entities],
+            [[relation.raw_uri, relation.label] for relation in item.sparql.relations]] for item in
+            dataset.train_set + dataset.test_set}
 
     @profile
     def generate(self, surface, question):
         if len(surface) > 2 and question in self.dataset:
-            return self.dataset[question]
+            return self.dataset[question][self.idx]
         return []
