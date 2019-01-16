2dab05f797f6d3550c55e9527bdaf7ce26855b20
hamidzafar <hamid.zafar.tud@gmail.com>
2019-01-16 17:36:10 +0100

    replace underline with space in uri label

--- common/dataset/container/uri.py
+++ common/dataset/container/uri.py
@@ -7,7 +7,7 @@ class URI:
         self.raw_uri = raw_uri.strip('<>')
         self.label = self.raw_uri[self.raw_uri.rindex('/') + 1:]
         self.tokens = re.sub(r"([A-Z])", r" \1", self.label).split()
-        self.label = self.label.lower()
+        self.label = self.label.replace('_', ' ').lower()
         self.ngram = None
         if ngram > 0:
             self.ngram = Utils.ngrams(self.label, ngram)
