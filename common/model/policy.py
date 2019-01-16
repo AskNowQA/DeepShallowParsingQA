3e80c5d66dfb48316aafcbbfde54465d5cd2d867
hamidzafar <hamid.zafar.tud@gmail.com>
2019-01-16 17:42:46 +0100

    set bias to True

--- common/model/policy.py
+++ common/model/policy.py
@@ -6,18 +6,19 @@ class Policy(nn.Module):
     def __init__(self, vocab_size, emb_size, input_size, hidden_size, output_size, dropout_ratio):
         super(Policy, self).__init__()
         self.output_size = output_size
+        bias = True
 
         self.emb = nn.Embedding(vocab_size, emb_size, padding_idx=0, sparse=False)
         self.emb.weight.requires_grad = False
 
-        self.layer1 = nn.Linear(input_size, hidden_size, bias=False)
+        self.layer1 = nn.Linear(input_size, hidden_size, bias=bias)
         self.activation1 = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout_ratio)
 
-        self.layer2 = nn.Linear(hidden_size, hidden_size // 2, bias=False)
+        self.layer2 = nn.Linear(hidden_size, hidden_size // 2, bias=bias)
         self.activation2 = nn.Sigmoid()
 
-        self.layer3 = nn.Linear(hidden_size // 2, output_size, bias=False)
+        self.layer3 = nn.Linear(hidden_size // 2, output_size, bias=bias)
         self.activation3 = nn.Softmax(dim=0)
 
     def forward(self, input):
