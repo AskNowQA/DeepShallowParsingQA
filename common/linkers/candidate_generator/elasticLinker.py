a02be29e1f26b984ff51b4d878d8901a8d81d9da
hamidzafar <hamid.zafar.tud@gmail.com>
2019-01-23 16:22:41 +0100

    take 100 results from elastic search

--- common/linkers/candidate_generator/elasticLinker.py
+++ common/linkers/candidate_generator/elasticLinker.py
@@ -3,5 +3,5 @@ class ElasticLinker:
         self.elastic = elastic
         self.index_name = index_name
 
-    def generate(self, surface, question, size=10):
+    def generate(self, surface, question, size=100):
         return self.elastic.search_index(surface, self.index_name, size=size)
