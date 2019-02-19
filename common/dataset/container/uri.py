66afa14a51052095d972967b172278c65c8a7115
hamidzafar <hamid.zafar.tud@gmail.com>
2019-02-19 10:29:29 +0100

    add coded proprty to URI which contains the word index and the length of label

--- common/dataset/container/uri.py
+++ common/dataset/container/uri.py
@@ -6,8 +6,9 @@ class URI:
     def __init__(self, raw_uri, ngram=0):
         self.raw_uri = raw_uri.strip('<>')
         self.label = self.raw_uri[self.raw_uri.rindex('/') + 1:]
-        self.tokens = re.sub(r"([A-Z])", r" \1", self.label).split()
+        self.tokens = [token.replace('.', '') for token in re.sub(r"([A-Z])", r" \1", self.label).split()]
         self.label = self.label.replace('_', ' ').lower()
+        self.coded = ([], 0)
         self.ngram = None
         if ngram > 0:
             self.ngram = Utils.ngrams(self.label, ngram)
