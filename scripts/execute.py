94cb21ebe5bea0740f8804ed65da614d4cca13db
hamidzafar <hamid.zafar.tud@gmail.com>
2019-02-19 10:33:41 +0100

    refactor to include QALD-7 dataset as well

--- scripts/execute.py
+++ scripts/execute.py
@@ -6,6 +6,7 @@ import time
 from config import config
 from scripts.config_args import parse_args
 from common.dataset.lc_quad import LC_QuAD
+from common.dataset.qald_7_ml import Qald_7_ml
 from common.model.runner import Runner
 
 np.random.seed(6)
@@ -25,13 +26,20 @@ if __name__ == '__main__':
 
     lc_quad = LC_QuAD(config['lc_quad']['train'], config['lc_quad']['test'], config['lc_quad']['vocab'],
                       False, args.remove_stop_words)
-    runner = Runner(lc_quad, args)
+    qald_7_ml = Qald_7_ml(config['qald_7_ml']['train'], config['qald_7_ml']['test'], config['qald_7_ml']['vocab'],
+                          False, False)
+    dataset = None
+    if args.dataset == 'lcquad':
+        dataset = lc_quad
+    elif args.dataset == 'qald_7_ml':
+        dataset = qald_7_ml
+    runner = Runner(dataset, args)
 
     if args.mode == 'test':
         runner.load_checkpoint()
     else:
-        runner.train(lc_quad, args)
+        runner.train(dataset, args)
     logger.setLevel(logging.DEBUG)
-    runner.test(lc_quad, args)
+    runner.test(dataset, args)
     finish = time.time()
     print('total runtime:', finish - start)
