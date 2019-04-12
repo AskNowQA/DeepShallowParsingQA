535a16a54a7328ec581dff074234e4c10d42d041
hamidzafar <hamid.zafar.tud@gmail.com>
2019-04-12 13:19:11 +0200

    add vocab code to output of one-hop relations

--- scripts/dataset_prepration/entity_one_hop.py
+++ scripts/dataset_prepration/entity_one_hop.py
@@ -1,14 +1,16 @@
-from tqdm import tqdm
+import argparse
+import re
+import torch
 import pickle as pk
 from config import config
 from common.dataset.lc_quad import LC_QuAD
 from common.dataset.qald_7_ml import Qald_7_ml
 from common.kb.dbpedia import DBpedia
-import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate one hop ')
     parser.add_argument('--dataset', default='lc_quad', help='`lc_quad` or `qald_7_ml`')
+    parser.add_argument('--max_length', default=3, type=int)
     args = parser.parse_args()
 
     dataset_name = args.dataset
@@ -19,21 +21,42 @@ if __name__ == '__main__':
     elif dataset_name == 'qald_7_ml':
         file_name = config['qald_7_ml']['entity_one_hop']
         dataset = Qald_7_ml(config['qald_7_ml']['train'], config['qald_7_ml']['test'], config['qald_7_ml']['vocab'],
-                          False, False)
+                            False, False)
     kb = DBpedia()
 
     one_hop = {}
-    for qa_row in tqdm(dataset.test_set):
-        for entity in qa_row.sparql.entities:
-            if entity.raw_uri not in one_hop:
-                relations = []
-                relations = kb.one_hop_relations(entity.raw_uri)
-                relations = [[item, item[item.rindex('/') + 1:]] for item in relations]
-                if relations is not None:
-                    one_hop[entity.raw_uri] = relations
-        with open(file_name, 'wb') as f:
-            pk.dump(one_hop, f)
+    # for qa_row in tqdm(dataset.test_set):
+    #     for entity in qa_row.sparql.entities:
+    #         if entity.raw_uri not in one_hop:
+    #             relations = []
+    #             relations = kb.one_hop_relations(entity.raw_uri)
+    #             relations = [[item, item[item.rindex('/') + 1:]] for item in relations]
+    #             if relations is not None:
+    #                 one_hop[entity.raw_uri] = relations
+    #     with open(file_name, 'wb') as f:
+    #         pk.dump(one_hop, f)
 
+    vocab = dataset.vocab
     with open(file_name, 'rb') as f:
         one_hop = pk.load(f)
+    for entity, uris in one_hop.items():
+        for idx in range(len(uris)):
+            uri, label = uris[idx][:2]
+            label = re.sub(r"([A-Z])", r" \1", label).replace('_', ' ').replace('.', ' ')
+            words = list(map(str.lower, label.split(' ')))
+            coded = vocab.convertToIdx(words, '')
+            coded_length = min(len(coded), args.max_length)
+            if len(coded) < args.max_length:
+                coded = coded + [0] * (args.max_length - len(coded))
+            else:
+                coded = coded[:args.max_length]
+            coded = torch.LongTensor(coded)
+            if len(uris[idx]) == 2:
+                uris[idx].extend([coded, coded_length])
+            else:
+                uris[idx][2] = coded
+                uris[idx][3] = coded_length
+    with open(file_name, 'wb') as f:
+        pk.dump(one_hop, f)
+
     print(len(one_hop))
