7c52347a05af6ba226f6421657070fa29936d8f2
hamidzafar <hamid.zafar.tud@gmail.com>
2019-06-14 11:34:59 +0200

    sort by the label, then by the last part of the URI

--- common/linkers/sorter/stringSimilaritySorter.py
+++ common/linkers/sorter/stringSimilaritySorter.py
@@ -13,20 +13,19 @@ class StringSimilaritySorter:
         if candidates is None or len(candidates) == 0:
             return []
         candidates_distance = np.array(
-            [(self.metric(surface, candidate[1].lower()))
+            [(self.metric(surface, candidate[1].lower()), self.metric(surface, candidate[0][28:].lower()))
              for candidate in candidates], dtype=float)
         exact_match_idx = [idx for idx, candidate in enumerate(candidates) if surface in candidate[1].lower()]
-        candidates_distance[exact_match_idx] /= 2
-        candidates_distance = candidates_distance
+        candidates_distance[exact_match_idx][:, 0] /= 2
         filtered_candidates = np.array(candidates, dtype=object)
-        idxs = np.argsort(candidates_distance)
+        idxs = np.lexsort((candidates_distance[:, 0], candidates_distance[:, 1]))
         if self.return_similarity:
             if self.metric_range_percentage:
-                candidates_similarity = 1 - candidates_distance
+                candidates_similarity = 1 - candidates_distance[:, 0]
             else:
                 surface_len = len(surface)
                 candidates_len = np.array([max(surface_len, len(candidate[1])) for candidate in candidates])
-                candidates_similarity = 1 - (candidates_distance / candidates_len)
+                candidates_similarity = 1 - (candidates_distance[:, 0] / candidates_len)
             output = np.hstack((filtered_candidates[idxs], candidates_similarity[idxs].reshape(-1, 1)))
         else:
             output = filtered_candidates[idxs]
