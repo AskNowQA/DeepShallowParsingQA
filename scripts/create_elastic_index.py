ca6e692cb51ab9d6f3adf54313ae3ed66ee57921
hamidzafar <hamid.zafar.tud@gmail.com>
2019-07-31 13:22:23 +0200

    fine tune labels

--- scripts/create_elastic_index.py
+++ scripts/create_elastic_index.py
@@ -41,12 +41,48 @@ if __name__ == '__main__':
                         op_dict = {"index": {"_index": args.index_name, "_type": 'resources'}}
                         bulk_data.append(op_dict)
                         bulk_data.append(data_dict)
+
+                    label = entity.raw_uri.encode('ascii', errors='ignore').decode()
+                    label = label[label.rindex('/') + 1:].replace('_', ' ')
+                    result = e.search_index(label, args.index_name)
+                    result = [item for item in result if item[0] == entity.raw_uri]
+                    if len(result) == 0:
+                        print(entity.raw_uri)
+                        data_dict = {'key': entity.raw_uri,
+                                     'dtype': 'uri',
+                                     'label': label}
+                        op_dict = {"index": {"_index": args.index_name, "_type": 'resources'}}
+                        bulk_data.append(op_dict)
+                        bulk_data.append(data_dict)
+
+                    if '_(' in entity.raw_uri:
+                        label = entity.label
+                        label = label[:label.index(' (')].replace('_', ' ')
+                        data_dict = {'key': entity.raw_uri,
+                                     'dtype': 'uri',
+                                     'label': label}
+                        op_dict = {"index": {"_index": args.index_name, "_type": 'resources'}}
+                        bulk_data.append(op_dict)
+                        bulk_data.append(data_dict)
+
             e.bulk_indexing(args.index_name, delete_index=False, index_config=index_config, bulk_data=bulk_data)
 
         elif 'relation' in args.index_name:
+            index_config = config['elastic']['relation_whole_match_index_config']
+            bulk_data = []
+            manual_list = [{'uri': 'http://dbpedia.org/ontology/TelevisionShow', 'label': 'show'}]
+            for item in manual_list:
+                data_dict = {'key': item['uri'],
+                             'dtype': 'uri',
+                             'label': item['label']
+                             }
+                op_dict = {"index": {"_index": args.index_name, "_type": 'resources'}}
+                bulk_data.append(op_dict)
+                bulk_data.append(data_dict)
+            e.bulk_indexing(args.index_name, delete_index=False, index_config=index_config, bulk_data=bulk_data)
             e.create_index(config['elastic']['relation_whole_match_index_config'],
-                                    config['dbpedia']['relations'],
-                                    index_name=args.index_name)
+                           config['dbpedia']['relations'],
+                           index_name=args.index_name)
             vocab = Vocab(filename=config['lc_quad']['vocab'], data=['<ent>', '<num>'])
 
             coded_labels = {}
